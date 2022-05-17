3415285960b6275e66f6d0e2cd611c17b1c5dbcf
Erik Hoej <erik@setsnail.com>
2022-05-17 23:39:27 +0200

    Remade the project using Unreal Engine

---
+++ Source/TD/TDGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TD_API ATDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
